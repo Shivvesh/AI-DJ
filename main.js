421f45c3b6e04d84977f2340c75fe0fad39b2f1e
Shivvesh <71574601+Shivvesh@users.noreply.github.com>
2021-09-18 12:32:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -31,6 +31,9 @@ function gotPoses(results)
     if(results.lenght > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = "+ leftWristX +"leftWristY = "+ leftWristY);
@@ -46,6 +49,19 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0, 0, 600, 650);
+
+    fill("#ADD8E6");
+    stroke("#ADD8E6");
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+		InNumberleftWristY = Number(leftWristY);
+		new_leftWristY = floor(InNumberleftWristY *2);
+		leftWristY_divide_1000 = new_leftWristY/1000;
+		document.getElementById("volume").innerHTML = "Volume = " + leftWristY_divide_1000;		
+		song.setVolume(leftWristY_divide_1000);	
+	}
 }
 
 function play()
